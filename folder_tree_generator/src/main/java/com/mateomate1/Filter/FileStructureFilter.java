a3b4e5ebff476e2bc22fe297e124cac82468d0c9
mateomate1 <mateoayarrabarbero@gmail.com>
2025-11-21 00:53:08 +0100

    update tree-generator

--- folder_tree_generator/src/main/java/com/mateomate1/Filter/FileStructureFilter.java
+++ folder_tree_generator/src/main/java/com/mateomate1/Filter/FileStructureFilter.java
@@ -1,10 +1,27 @@
-package com.mateomate1;
+package com.mateomate1.Filter;
 
 import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * File filter that allows including or excluding files based on their
+ * names or extensions. Directories are accepted unless their name is
+ * explicitly excluded.
+ *
+ * The filter supports four optional lists:
+ * - includedExtensions: only files with these extensions will be accepted
+ * - excludedExtensions: files with these extensions will be rejected
+ * - includedFiles: file names that are always accepted
+ * - excludedFiles: file names that are always rejected
+ *
+ * If multiple lists are configured, the logic inside accept() determines
+ * the priority between them.
+ *
+ * @author mateomate1
+ * @version 1.0.0
+ */
 public class FileStructureFilter implements FileFilter {
 
     private List<String> excludedExtensions = null;
@@ -13,9 +30,18 @@ public class FileStructureFilter implements FileFilter {
     private List<String> includedFiles = null;
 
     /**
-     * Metodo que revisa cual de las listas esta rellena para asi decidir cuando no
-     * incluir el archivo recivido
+     * Decides whether a given File should be accepted based on the configured
+     * extension and file name rules.
      *
+     * Logic summary:
+     * - Directories are accepted unless their name is inside excludedFiles.
+     * - If includedExtensions is defined, a file must match one of them.
+     * - If excludedExtensions is defined, files matching one of them are rejected,
+     * unless includedFiles explicitly contains the file name.
+     * - File name lists (includedFiles and excludedFiles) override extension rules.
+     *
+     * @param f file to evaluate
+     * @return true if the file is accepted, false otherwise
      */
     @Override
     public boolean accept(File f) {
@@ -42,38 +68,83 @@ public class FileStructureFilter implements FileFilter {
         return true;
     }
 
+    /**
+     * Gets the list of excluded extensions.
+     *
+     * @return list of extensions that should be excluded, or null if not defined
+     */
     public List<String> getExcludedExtensions() {
         return excludedExtensions;
     }
 
+    /**
+     * Sets the list of extensions that should be excluded.
+     *
+     * @param excludedExtensions list of extensions to exclude
+     */
     public void setExcludedExtensions(List<String> excludedExtensions) {
         this.excludedExtensions = excludedExtensions;
     }
 
+    /**
+     * Gets the list of included extensions.
+     *
+     * @return list of extensions that should be included, or null if not defined
+     */
     public List<String> getIncludedExtensions() {
         return includedExtensions;
     }
 
+    /**
+     * Sets the list of extensions that should be included.
+     *
+     * @param includedExtensions list of extensions to include
+     */
     public void setIncludedExtensions(List<String> includedExtensions) {
         this.includedExtensions = includedExtensions;
     }
 
+    /**
+     * Gets the list of excluded file names.
+     *
+     * @return list of file names that should be excluded, or null if not defined
+     */
     public List<String> getExcludedFiles() {
         return excludedFiles;
     }
 
+    /**
+     * Sets the list of file names that should be excluded.
+     *
+     * @param excludedFiles list of file names to exclude
+     */
     public void setExcludedFiles(List<String> excludedFiles) {
         this.excludedFiles = excludedFiles;
     }
 
+    /**
+     * Gets the list of included file names.
+     *
+     * @return list of file names that should be included, or null if not defined
+     */
     public List<String> getIncludedFiles() {
         return includedFiles;
     }
 
+    /**
+     * Sets the list of file names that should be included.
+     *
+     * @param includedFiles list of file names to include
+     */
     public void setIncludedFiles(List<String> includedFiles) {
         this.includedFiles = includedFiles;
     }
 
+    /**
+     * Adds an extension to the list of excluded extensions.
+     *
+     * @param ext extension to exclude
+     */
     public void addExcludedExtension(String ext) {
         if (excludedExtensions == null) {
             excludedExtensions = new ArrayList<>();
@@ -81,6 +152,11 @@ public class FileStructureFilter implements FileFilter {
         excludedExtensions.add(ext);
     }
 
+    /**
+     * Adds an extension to the list of included extensions.
+     *
+     * @param ext extension to include
+     */
     public void addIncludedExtension(String ext) {
         if (includedExtensions == null) {
             includedExtensions = new ArrayList<>();
@@ -88,6 +164,11 @@ public class FileStructureFilter implements FileFilter {
         includedExtensions.add(ext);
     }
 
+    /**
+     * Adds a file name to the list of excluded files.
+     *
+     * @param file file name to exclude
+     */
     public void addExcludedFile(String file) {
         if (excludedFiles == null) {
             excludedFiles = new ArrayList<>();
@@ -95,6 +176,11 @@ public class FileStructureFilter implements FileFilter {
         excludedFiles.add(file);
     }
 
+    /**
+     * Adds a file name to the list of included files.
+     *
+     * @param file file name to include
+     */
     public void addIncludedFile(String file) {
         if (includedFiles == null) {
             includedFiles = new ArrayList<>();
@@ -102,6 +188,11 @@ public class FileStructureFilter implements FileFilter {
         includedFiles.add(file);
     }
 
+    /**
+     * Adds multiple extensions to the list of excluded extensions.
+     *
+     * @param list list of extensions to exclude
+     */
     public void addExcludedExtensions(List<String> list) {
         if (excludedExtensions == null) {
             excludedExtensions = new ArrayList<>();
@@ -109,6 +200,11 @@ public class FileStructureFilter implements FileFilter {
         excludedExtensions.addAll(list);
     }
 
+    /**
+     * Adds multiple extensions to the list of included extensions.
+     *
+     * @param list list of extensions to include
+     */
     public void addIncludedExtensions(List<String> list) {
         if (includedExtensions == null) {
             includedExtensions = new ArrayList<>();
@@ -116,6 +212,11 @@ public class FileStructureFilter implements FileFilter {
         includedExtensions.addAll(list);
     }
 
+    /**
+     * Adds multiple file names to the list of excluded files.
+     *
+     * @param list list of file names to exclude
+     */
     public void addExcludedFiles(List<String> list) {
         if (excludedFiles == null) {
             excludedFiles = new ArrayList<>();
@@ -123,6 +224,11 @@ public class FileStructureFilter implements FileFilter {
         excludedFiles.addAll(list);
     }
 
+    /**
+     * Adds multiple file names to the list of included files.
+     *
+     * @param list list of file names to include
+     */
     public void addIncludedFiles(List<String> list) {
         if (includedFiles == null) {
             includedFiles = new ArrayList<>();
@@ -130,6 +236,12 @@ public class FileStructureFilter implements FileFilter {
         includedFiles.addAll(list);
     }
 
+    /**
+     * Loads patterns from a .gitignore file and adds them to the filter.
+     *
+     * @implNote implement gitignore parsing and list population
+     * @hidden
+     */
     public void addExcludeFromGitignore() {
         // TODO: hacer la implementacion
     }
