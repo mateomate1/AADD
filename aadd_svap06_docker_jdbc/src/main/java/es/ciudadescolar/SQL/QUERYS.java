76e32cb05a0ea347ac90dc9a0b6054cdafe82e32
mateomate1 <mateoayarrabarbero@gmail.com>
2025-12-10 09:50:14 +0100

    update

--- aadd_svap06_docker_jdbc/src/main/java/es/ciudadescolar/SQL/QUERYS.java
+++ aadd_svap06_docker_jdbc/src/main/java/es/ciudadescolar/SQL/QUERYS.java
@@ -1,11 +1,14 @@
 package es.ciudadescolar.SQL;
 
 public class QUERYS {
-    // Selects _______________________________________________________________________________________________
+    // Selects
+    // _______________________________________________________________________________________________
 
-    // Inserts _______________________________________________________________________________________________
-    private static final String insertPeliculas = "INSERT INTO film(film_id, title, language_id, rental_duration, rental_rate, replacement_cost, last_update) VALUES(?, ?, ?, ?, ?, ?, ?)";
-    
-    // Deletes _______________________________________________________________________________________________
-
+    // Inserts
+    // _______________________________________________________________________________________________
+    private static final String INSERT_FILM = "INSERT INTO film(film_id, title, language_id, rental_duration, rental_rate, replacement_cost, last_update) VALUES(?, ?, ?, ?, ?, ?, ?)";
+    private static final String INSERT_ACTOR = "INSERT INTO actor(actor_id, first_name, last_name, last_update) VALUES(?, ?, ?, ?)";
+    private static final String INSERT_FILM_ACTOR = "INSERT INTO film_actor(actor_id, film_id, last_update) VALUES(?, ?, ?)";
+    // Deletes
+    // _______________________________________________________________________________________________
 }
