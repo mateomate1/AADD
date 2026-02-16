01198d1c73d0b3803f33c0f50693abda0392aaf9
mateomate1 <mateoayarrabarbero@gmail.com>
2026-02-16 11:39:58 +0100

    update

---
+++ navidad25_rest/src/main/java/es/ciudadescolar/navidad25_rest/Navidad25RestApplication.java
@@ -0,0 +1,13 @@
+package es.ciudadescolar.navidad25_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Navidad25RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Navidad25RestApplication.class, args);
+	}
+
+}
---
+++ navidad25_rest/src/test/java/es/ciudadescolar/navidad25_rest/Navidad25RestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ciudadescolar.navidad25_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Navidad25RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
